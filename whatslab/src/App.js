3af4deacb2863016f6907873ff5798d728da83a9
José <josevictortf@gmail.com>
2020-12-18 10:59:32 -0300

    Criado os arquivos necessários e adicionado os inputs

---
+++ whatslab/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Messages from './components/Messages/Messages'
+import Inputs from './components/Input/Inputs'
+function App() {
+  return (
+    <div className="App">
+      <Inputs />
+    </div>
+  );
+}
+
+export default App;
